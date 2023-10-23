24c0c4e85cf357086105cd0d7cd5ad0712531e9f
LinuY <linnuy@foxmail.com>
2023-10-23 20:29:41 +0800

    add LC2678

---
+++ src/main/java/space/linuy/leetcode/LC2678.java
@@ -0,0 +1,18 @@
+package space.linuy.leetcode;
+
+public class LC2678 {
+
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for (String detail : details) {
+            if (Integer.parseInt(detail.substring(11, 12)) * 10 + Integer.parseInt(detail.substring(12, 13)) > 60) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LC2678().countSeniors(new String[] {"7868190130M7522","5303914400F9211","9273338290F4010"}));
+    }
+}
