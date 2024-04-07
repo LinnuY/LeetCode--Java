385d8e7dfdcba60452b9bf351ac13709cc3d25cd
LinnuY <linnuy@foxmail.com>
2024-04-08 00:50:05 +0800

    2024/04/07

--- src/main/java/space/linuy/leetcode/LC10.java
+++ src/main/java/space/linuy/leetcode/LC10.java
@@ -2,30 +2,8 @@ package space.linuy.leetcode;
 
 public class LC10 {
     public boolean isMatch(String s, String p) {
-        int i, j;
-        for (i = 0, j = 0; i < s.length() && j < p.length();) {
-            if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.' || p.charAt(j) == '*') {
-                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
-                    i++;
-                    j++;
-                } else {
-                    if (p.charAt(j - 1) == s.charAt(i) || p.charAt(j - 1) == '.') {
-                        i++;
-                    } else if (p.charAt(j + 1) == s.charAt(i) || p.charAt(j + 1) == '.') {
-                        i++;
-                        j += 2;
-                    } else {
-                        return false;
-                    }
-                }
-            } else {
-                j++;
-                if (p.charAt(j) == '*') {
-                    j++;
-                }
-            }
-        }
-        return i == s.length();
+        p = p.replace("***", "*");
+        return s.matches(p);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/space/linuy/leetcode/LC11.java
@@ -0,0 +1,18 @@
+package space.linuy.leetcode;
+
+public class LC11 {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            int result = Math.min(height[i], height[j]) * (j - i);
+            max = Math.max(max, result);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+}
